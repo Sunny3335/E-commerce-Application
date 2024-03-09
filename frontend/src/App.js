209ba6d455e7c5f13c8b8b29368bbec1b446cc7e
sunny kumar <sunnykumar2020has@gmail.com>
2024-03-09 22:59:54 +0530

    changes saved

---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+import {BrowserRouter, Routes,  Route} from 'react-router-dom'
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
+import LoginSignup from './Pages/LoginSignup';
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<shop/>}></Route>
+          <Route path='/mens' element={<shopCategory category="men"/>}></Route>
+          <Route path='/womens' element={<shopCategory category="women"/>}></Route>
+          <Route path='/kids' element={<shopCategory category="kid" />}></Route>
+          <Route path='/product' element={<Product />}>
+            <Route path=':productId' element={<Product/>} />
+          </Route>
+          <Route path='/cart' element={<Cart/>} />
+          <Route path='/login' element={<LoginSignup/>}/>
+
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Pages/shopCategory.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const shopCatogery = () => {
+  return (
+    <div>shopCatogery</div>
+  )
+}
+
+export default shopCatogery
